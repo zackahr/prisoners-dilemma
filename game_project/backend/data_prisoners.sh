29baaddef291d8fcf4c53826e170b2d965526b1f
zackahr <ziko.isezzine@gmail.com>
2025-06-24 22:00:22 +0000

    .

--- game_project/backend/data_prisoners.sh
+++ game_project/backend/data_prisoners.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 set -a
-source ../.env
+source ../.env  # Correct path to .env file
 set +a
 
-docker-compose exec -T db psql -U postgres -d "$DB_NAME" < "../$SQL_FILE_PRISONERS" > output_data.csv
+docker compose exec -T db psql -U postgres -d "$DB_NAME" < "../$SQL_FILE_PRISONERS" > output_data.csv
 
 echo "SQL query executed, output saved to output_data.csv"
